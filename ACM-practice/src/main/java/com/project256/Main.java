19e74ba73c143b4bfe0ff2257ea6ecf3c0bdefcb
Lin <lxc175322@alibaba-inc.com>
2025-08-20 11:33:23 +0800

    rm idea

---
+++ ACM-practice/src/main/java/com/project256/Main.java
@@ -0,0 +1,11 @@
+package com.project256.quant;
+
+/**
+ * @author Xingchen.Lin
+ * @date 2025/8/20 11:32
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
